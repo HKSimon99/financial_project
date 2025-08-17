a621347c4b8c79695d5e5d0a97bb0d758cbd762a
HKSimon99 <kyusung1234@naver.com>
2025-08-17 23:41:43 +0900

    Next.js move

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,15 @@
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = withPWA({
+  i18n: {
+    locales: ['ko', 'en'],
+    defaultLocale: 'en',
+  },
+});
+
+export default nextConfig;
